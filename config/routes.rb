0fb6c4919732f9d306bfe20330f436a37c2b2332
Alexander Haumer <alexander.haumer@me.com>
2021-05-25 11:03:05 +0100

    raisl crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants
+  # # READ ALL
+  # get "restaurants", to: "restaurants#index"
+  # # restaurants_path
+
+  # # CREATE (FORM)
+  # get "restaurants/new", to: "restaurants#new", as: "new_restaurant"
+  # # new_restaurant_path
+
+  # # CREATE (AFFECT DB)
+  # post "restaurants", to: "restaurants#create"
+
+  # # READ ONE
+  # get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  # # restaurant_path(:id)
+
+  # # UPDATE (FORM)
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: "edit_restaurant"
+
+  # # UPDATE (AFFECT DB)
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # DELETE
+  # delete "restaurants/:id", to: "restaurants#destroy"
 end
